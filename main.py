a4c6988d5db699f82bfef14bff2aeb2124120ce6
dberweger2017 <94843489+dberweger2017@users.noreply.github.com>
2022-02-02 20:49:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 st.title("Calculadora")
 choice = st.selectbox("Que quieres hacer?", ["Suma", "Resta", "Multiplicacion", "Division", "Media"])
-num1 = st.number_input("Escge un primer numero:", int)
-num2 = st.number_input("Escoge u segundo numero:")
+num1 = st.number_input("Escge un primer numero:", step = 1)
+num2 = st.number_input("Escoge u segundo numero:", step = 1)
 
 resultado = None
 if st.button("Calcular"):
@@ -11,7 +11,7 @@ if st.button("Calcular"):
          resultado = num1 + num2
      elif choice == "resta":
          resultado = num1 - num2
-    
-     
+
+
      st.write("El resultado es " + str(resultado))
 #def calculadora()
