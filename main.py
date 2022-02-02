fb05258256ac929429510e1a1e87c0aab72834c0
dberweger2017 <94843489+dberweger2017@users.noreply.github.com>
2022-02-02 21:26:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import streamlit as st
 resultado = None
 num1 = None
 num2 = None
+numMedia = []
 
 st.title("Calculadora")
 choice = st.selectbox("Que quieres hacer?", ["Suma", "Resta", "Multiplicacion", "Division", "Media"])
@@ -12,8 +13,9 @@ if choice == "Suma" or choice == "Resta" or choice == "Multiplicacion" or choice
     num1 = st.number_input("Escge un primer numero:", step=1)
     num2 = st.number_input("Escoge u segundo numero:", step=1)
 elif choice == "Media":
-    pass
-
+    numMediaImput = st.slider("De cuantos numeros quieres hacer la media?", min_value=2, max_value=20, value = 5)
+    for i in range(numMediaImput):
+        numMedia += [st.number_input("Escge un primer numero:", step=1, key = i)]
 
 #Calculo
 if st.button("Calcular"):
@@ -25,5 +27,10 @@ if st.button("Calcular"):
         resultado = num1 * num2
     elif choice == "Division":
         resultado = num1/num2
+    elif choice == "Media":
+        resultado = 0
+        for i in range(len(numMedia)):
+            resultado += numMedia[i]
+        resultado = resultado / len(numMedia)
 
     st.write("El resultado es " + str(resultado))
